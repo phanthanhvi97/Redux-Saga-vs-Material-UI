785c2284980eb14780de9d37752ee666f9e8c5d3
phanthanhvi97 <1660710@student.hcmus.edu.vn>
2019-09-22 21:56:48 +0700

    no message

--- src/components/TaskForm/index.js
+++ src/components/TaskForm/index.js
@@ -1,44 +1,46 @@
 import React, { Component } from 'react'
-import { withStyles } from '@material-ui/core'
+import { withStyles, Modal, Grid, Button, Box } from '@material-ui/core'
 import styles from './styles'
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField';
 
 class TaskForm extends Component {
     render() {
         const { open, classes, onClose } = this.props
         return (
-            <Dialog open={open} onClose={onClose} aria-labelledby="form-dialog-title">
-                <DialogTitle id="form-dialog-title">Thêm mới công việc</DialogTitle>
-                <DialogContent>
-                    <TextField
-                        id="standard-name"
-                        label="Name"
-                        className={classes.textField}
-                        margin="normal"
-                    />
-                    <TextField
-                        id="standard-multiline-flexible"
-                        label="Multiline"
-                        multiline
-                        rowsMax="4"
-                        className={classes.textField}
-                        margin="normal"
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={onClose} color="primary">
-                        Cancel
-                    </Button>
-                    <Button onClick={onClose} color="primary">
-                        OK
-                    </Button>
-                </DialogActions>
-            </Dialog>
+            <Modal open={open} onClose={onClose}>
+                <div className={classes.modal}>
+                    <form>
+                        <Grid container spacing={4}>
+                            <Grid item md={12}>
+                                <TextField
+                                    id="standard-name"
+                                    label="Tieu de"
+                                    className={classes.textField}
+                                    margin="normal"
+                                />
+                            </Grid>
+                            <Grid item md={12}>
+                                <TextField
+                                    id="standard-multiline-flexible"
+                                    label="Mo ta"
+                                    multiline
+                                    rowsMax="4"
+                                    className={classes.textField}
+                                    margin="normal"
+                                />
+                            </Grid>
+                            <Grid item md={12}>
+                                <Box display="flex" flexDirection="row-reverse" mt={1}>
+                                    <Button variant="contained" color="primary">Save</Button>
+                                    <Box mr={1}>
+                                        <Button variant="contained" color="secondary" onClick={onClose}>Cancel</Button>
+                                    </Box>
+                                </Box>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+            </Modal>
         )
     }
 }
