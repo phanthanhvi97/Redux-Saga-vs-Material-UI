9b402b7c2156135ab8f722f380b2de81c2fe586c
Phan Thanh Vi <1660710@student.hcmus.edu.vn>
2019-10-01 15:27:51 +0700

    xong142

--- src/containers/TaskForm/index.js
+++ src/containers/TaskForm/index.js
@@ -2,38 +2,46 @@ import { Box, Button, Grid, withStyles } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import React, { Component } from 'react';
 import styles from './styles';
-import {compose, bindActionCreators} from 'redux'
+import { compose, bindActionCreators } from 'redux'
 import * as modalActions from '../../actions/modal'
-import {connect} from 'react-redux'
-
+import { connect } from 'react-redux'
+import { Field, reduxForm } from 'redux-form'
+import renderTextField from '../../components/FormHelper/TextField'
 class TaskForm extends Component {
+    handleSubmitForm = data => {
+        console.log(data)
+    }
     render() {
-        const { classes, modalActionCreators} = this.props
-        const {hideModal} = modalActionCreators
+        const { classes, modalActionCreators, handleSubmit } = this.props
+        const { hideModal } = modalActionCreators
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.handleSubmitForm)}>
                 <Grid container spacing={4}>
                     <Grid item md={12}>
-                        <TextField
-                            id="standard-name"
+                        <Field
+                            id="title"
                             label="Tieu de"
                             className={classes.textField}
                             margin="normal"
+                            name="title"
+                            component={renderTextField}
                         />
                     </Grid>
                     <Grid item md={12}>
-                        <TextField
-                            id="standard-multiline-flexible"
+                        <Field
+                            id="description"
                             label="Mo ta"
                             multiline
-                            rowsMax="4"
+                            rowsmax="4"
                             className={classes.textField}
                             margin="normal"
+                            component={renderTextField}
+                            name="description"
                         />
                     </Grid>
                     <Grid item md={12}>
                         <Box display="flex" flexDirection="row-reverse" mt={1}>
-                            <Button variant="contained" color="primary">Save</Button>
+                            <Button variant="contained" color="primary" type='submit'>Save</Button>
                             <Box mr={1}>
                                 <Button variant="contained" color="secondary" onClick={hideModal}>Cancel</Button>
                             </Box>
@@ -44,8 +52,12 @@ class TaskForm extends Component {
         )
     }
 }
-const mapDispatchToProps=dispatch=>({
-    modalActionCreators:bindActionCreators(modalActions,dispatch)
+const mapDispatchToProps = dispatch => ({
+    modalActionCreators: bindActionCreators(modalActions, dispatch)
 })
-const withConnect=connect(null,mapDispatchToProps)
-export default compose(withStyles(styles), withConnect)(TaskForm)
+const FORM_NAME = 'TASK_MANAGEMENT'
+const withConnect = connect(null, mapDispatchToProps)
+const withReduxForm = reduxForm({
+    form: FORM_NAME
+})
+export default compose(withStyles(styles), withConnect, withReduxForm)(TaskForm)
